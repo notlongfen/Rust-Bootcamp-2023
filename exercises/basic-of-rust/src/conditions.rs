c86aebab1948ed37a8c5ec80cd1a8f84d7041626
notlongfen <root@DESKTOP-KMNS09Q>
2023-07-09 19:10:33 +0700

    Sua thanh code cua toi

--- exercises/basic-of-rust/src/conditions.rs
+++ exercises/basic-of-rust/src/conditions.rs
@@ -4,26 +4,35 @@
 // - another function call
 // - additional variables
 pub fn bigger(a: i32, b: i32) -> i32 {
-    todo!()
+    if a>b{
+        return a;
+    }else{
+        return b;
+    }
 }
 
 //Exercise 2
 // Input: Provide an arbitrary value of number
 // Check number is Positive or Negative or Zero
 // Output: &str
-fn check_number(number: u32) -> &'static str {
-    todo!()
+fn check_number(number: i32) -> &'static str {
+    if number > 0 {
+        "Positive"
+    } else if number < 0 {
+        "Negative"
+    } else {
+        "Zero"
+    }
 }
 
 // Exercise 3
 // Step 1: Make me compile!
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
-
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
-        "foo"
+        "bar"
     } else {
-        1
+        "baz"
     }
 }
 
@@ -31,14 +40,21 @@ pub fn foo_if_fizz(fizzish: &str) -> &str {
 // Determine if a given year is a leap year
 // Implement logic
 fn is_leap_year(year: i32) -> bool {
-    todo!()
+    if year % 4 ==0 && year % 100 != 0 || year % 400 == 0{
+        return true;
+    }  
+    return false;
 }
 
 // Exercise 5
 // Calculate the factorial of a number
 // Implement logic
 fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut res: u32=1;
+    for i in 1..=n {
+        res*= i;
+    }
+    res
 }
 
 // Exercise 6
@@ -46,7 +62,16 @@ fn factorial(n: u32) -> u32 {
 // Implement logic
 
 fn is_prime(n: u32) -> bool {
-    todo!()
+    if n==1 {return false}
+    if n==0 {return false}
+    for i in 2..(n / 2 + 1) {
+        if n % i == 0 {
+            return false;
+        }
+    }
+
+    true
+
 }
 
 
@@ -87,13 +112,13 @@ mod tests {
     // Test for exercise 3
     #[test]
     fn foo_for_fizz() {
-        assert_eq!(foo_if_fizz("fizz"), "foo")
+        assert_eq!(foo_if_fizz("fizz"), "bar")
     }
 
     // Test for exercise 3
     #[test]
     fn bar_for_fuzz() {
-        assert_eq!(foo_if_fizz("fuzz"), "bar")
+        assert_eq!(foo_if_fizz("fuzz"), "baz")
     }
 
     // Test for exercise 3
--- exercises/basic-of-rust/src/strings.rs
+++ exercises/basic-of-rust/src/strings.rs
@@ -1,24 +1,25 @@
 // Exercise 1
 #[allow(dead_code)]
 fn exercise1(color: &str) -> String {
-    todo!()
+    let colors = color.to_string();
+    colors
 }
 
 // Exercise 2
 // Fix all errors without adding newline
 fn exercise2() -> String {
-    let s = String::from("hello");
+    let mut s: String = String::from("hello");
     s.push(',');
-    s.push(" world");
-    s += "!".to_string();
+    s.push_str(" world");
+    s += &"!".to_string();
     s
 }
 // Exercise 3
 // Fix errors without removing any line
 fn exercise3() -> String {
     let s1 = String::from("hello,");
-    let s2 = String::from("world!");
-    let s3 = s1 + s2;
+    let s2 = String::from(" world!");
+    let s3 = s1 + &s2;
     s3
 }
 
@@ -26,20 +27,37 @@ fn exercise3() -> String {
 // Reverse a string
 
 fn reverse_string(input: &str) -> String {
-    todo!()
+    let mut res: String = String::new();
+    for i in (0..input.len()).rev(){
+        let ch = input.chars().nth(i).unwrap();
+        res.push(ch);
+    }
+    res
 }
 
 
 // Exercise 5
 // Check if a string is a palindrome
 fn is_palindrome(word: &str) -> bool {
-    todo!()
+    let len = word.len();
+    for i in 0..len / 2 {
+        if word.chars().nth(i).unwrap().to_lowercase().next().unwrap() != word.chars().nth(len - i - 1).unwrap().to_lowercase().next().unwrap() {
+            return false;
+        }
+    }
+    true
 }
 
 // Exercise 6
 // Count the occurrences of a character in a string
 fn count_char_occurrences(string: &str, ch: char) -> usize {
-    todo!()
+    let mut count = 0;
+    for i in string.chars(){
+        if i == ch{
+            count+=1;
+        }
+    }
+    count
 }
 
 #[cfg(test)]
@@ -92,7 +110,7 @@ mod tests {
     #[test]
     fn test_count_char_occurrences() {
         assert_eq!(count_char_occurrences("Hello", 'l'), 2);
-        assert_eq!(count_char_occurrences("Rust is fun", 'u'), 1);
+        assert_eq!(count_char_occurrences("Rust is fun", 'u'), 2);
         assert_eq!(count_char_occurrences("Mississippi", 's'), 4);
     }
 
