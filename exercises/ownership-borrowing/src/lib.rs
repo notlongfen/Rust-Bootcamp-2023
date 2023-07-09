b80ae780b0e001b71fafb6c951fdc3338bd3a030
notlongfen <root@DESKTOP-KMNS09Q>
2023-07-09 19:50:10 +0700

    Rust lesson 2

--- exercises/ownership-borrowing/src/lib.rs
+++ exercises/ownership-borrowing/src/lib.rs
@@ -3,8 +3,9 @@
 fn exercise1() {
     // Use as many approaches as you can to make it work
     let x = String::from("hello, world");
-    let y = x;
-    let z = x;
+    let y = &x;
+    let z = &x;
+    print!("{},{}",y,z);
 }
 
 // Exercise 2
@@ -17,8 +18,9 @@ fn exercise2() {
     println!("{}", s2);
 }
 // Only modify the code below!
-fn take_ownership(s: String) {
+fn take_ownership(s: String) -> String {
     println!("{}", s);
+    s
 }
 
 // Exercise 3
@@ -41,7 +43,7 @@ fn exercise3() {
         let mut addition: f64 = 0.0;
 
         // Sumar valores en additions
-        for element_index in additions {
+        for &element_index in &additions {
             let addition_aux = values[element_index];
             addition = addition_aux + addition;
         }
@@ -50,10 +52,10 @@ fn exercise3() {
 
 // Exercise 4
 // Make it compile
-fn exercise4(value: u32) -> &'static str {
+fn exercise4(value: u32) -> String {
     let str_value = value.to_string(); // Convert u32 to String
-    let str_ref: &str = &str_value; // Obtain a reference to the String
-    str_ref // Return the reference to the String
+    // Obtain a reference to the String
+    str_value // Return the reference to the String
 }
 
 // Exercise 5
@@ -61,16 +63,17 @@ fn exercise4(value: u32) -> &'static str {
 use std::collections::HashMap;
 fn exercise5() {
     let mut my_map = HashMap::from([(1, "1.0".to_string()), (2, "2.0".to_string())]);
-
+    let tmp;
     let key = 3;
-
     let res = match my_map.get(&key) {
         Some(child) => child,
         None => {
             let value = "3.0".to_string();
-            my_map.insert(key, value);
-            &value // HERE IT FAILS
+            my_map.insert(key, value.clone());
+            tmp=value;
+            &tmp // HERE IT FAILS
         }
+
     };
 
     println!("{}", res);
@@ -82,14 +85,13 @@ fn exercise5() {
 use std::io;
 
 fn exercise6() {
-    let mut prev_key: &str = "";
-
+    let mut prev_key: String = String::new();
+    
     for line in io::stdin().lines() {
         let s = line.unwrap();
-
         let data: Vec<&str> = s.split("\t").collect();
         if prev_key.len() == 0 {
-            prev_key = data[0];
+            prev_key = data[0].to_string();
         }
     }
 }
@@ -102,30 +104,30 @@ fn exercise7() {
         let chars = [b'x', b'y', b'z'];
         let s: &str = std::str::from_utf8(&chars).unwrap();
         v.push(&s);
+        println!("{:?}", v);
     }
-    println!("{:?}", v);
-}
+} 
 
 // Exercise 8
 // Make it compile
 fn exercise8() {
-    let mut accounting = vec!["Alice", "Ben"];
+    let mut accounting = vec!["Alice".to_string(), "Ben".to_string()];
     
     loop {
-        let mut add_input = String::from("");
+        let mut add_input = String::new();
 
         io::stdin()
             .read_line(&mut add_input)
             .expect("Failed to read line");
 
         let add_vec: Vec<&str> = add_input.trim()[..].split_whitespace().collect();
-
+        
         if add_vec.len() < 1 {
             println!("Incorrect input, try again");
             continue;
         }
-
+        
         let person = add_vec[0];
-        accounting.push(person);
+        accounting.push(person.to_string());
     }
 }
